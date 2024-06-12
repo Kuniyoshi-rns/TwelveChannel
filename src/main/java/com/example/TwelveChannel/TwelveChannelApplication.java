beea9d834816b42ac06ba2c38ba1d4615c5fc5fd
Kuniyoshi <24kuniyoshi_takuma@axiz.co.jp>
2024-06-12 14:15:07 +0900

    start

---
+++ src/main/java/com/example/TwelveChannel/TwelveChannelApplication.java
@@ -0,0 +1,13 @@
+package com.example.TwelveChannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwelveChannelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwelveChannelApplication.class, args);
+	}
+
+}
