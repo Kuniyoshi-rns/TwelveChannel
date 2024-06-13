dded360052ebbb1cdeb1333a879ba45d678a8880
higa <higaryuki0408@gmail.com>
2024-06-13 16:23:45 +0900

    0613-threadパッケージ作成

--- src/main/java/com/example/TwelveChannel/Thread/IThreadService.java
+++ src/main/java/com/example/TwelveChannel/Thread/IThreadService.java
@@ -1,4 +1,18 @@
 package com.example.TwelveChannel.Thread;
 
+import java.util.List;
+
 public interface IThreadService {
+
+    public List<ThreadEntity> TreadAll();
+
+    public ThreadEntity FindbyIdthread(int thread_id);
+
+    public int InsertThread(ThreadEntity threadEntity);
+
+    public int UpdateThread(ThreadEntity threadEntity);
+
+    public int DeleteThread(int thread_id);
+
+    public void AddViewCount(int id);
 }
