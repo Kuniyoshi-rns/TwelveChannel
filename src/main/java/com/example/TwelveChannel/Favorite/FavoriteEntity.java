b1c0fb0c610ba71c4c431202a6f1d7c401394b06
unten <24unten_miku@axiz.co.jp>
2024-06-14 11:59:04 +0900

    0614-favoriteパッケージの作成

---
+++ src/main/java/com/example/TwelveChannel/Favorite/FavoriteEntity.java
@@ -0,0 +1,4 @@
+package com.example.TwelveChannel.Favorite;
+
+public record FavoriteEntity(int thread_id,int user_id) {
+}
---
+++ src/main/java/com/example/TwelveChannel/Favorite/count.java
@@ -0,0 +1,3 @@
+package com.example.TwelveChannel.Favorite;
+
+public record count(int count) {}
